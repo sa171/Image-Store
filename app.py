832578f6126a63db8595361afc02a25dfdfbeeca
sa171 <rudrawars@gmail.com>
2022-09-19 11:33:47 -0700

    @sa171 added exception handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from PIL import Image
 import uuid,os
 
@@ -13,18 +13,24 @@ def process_image():
     file = request.files['image']
     cate = request.form['category']
     # Read the image via file.stream and save it in directory
-    img = Image.open(file.stream)
-    filename = str(uuid.uuid4())
-    filepath = os.path.join(os.getcwd(),cate)
-    if(os.path.exists(filepath)):
-        filepath = os.path.join(filepath,filename)+".jpg"
-        img.save(filepath)
-    else:
-        os.mkdir(filepath)
-        filepath = os.path.join(filepath,filename)+".jpg"
-        img.save(filepath)
-
-    return jsonify({'msg': 'success', 'size': [img.width, img.height],'cat': cate})
+    try:
+        img = Image.open(file.stream)
+        filename = str(uuid.uuid4())
+        filepath = os.path.join(os.getcwd(),cate)
+        if(os.path.exists(filepath)):
+            filepath = os.path.join(filepath,filename)+".jpg"
+            img.save(filepath)
+        else:
+            os.mkdir(filepath)
+            filepath = os.path.join(filepath,filename)+".jpg"
+            img.save(filepath)
+        raise Exception("Trial exception")
+        return jsonify({'msg': 'success', 'size': [img.width, img.height],'cat': cate})
+    except :
+        return Response(
+            "some issue with the server",
+            status=500,
+        )
 
 if __name__ == "__main__":
   app.run()
