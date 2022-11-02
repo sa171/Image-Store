908f1f61ea01d6aa1281920cb29a8664de8ed43a
sa171 <rudrawars@gmail.com>
2022-11-01 19:11:08 -0700

    @sa171 added functionality for reverting image

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, Response
-from PIL import Image
+from PIL import Image, ImageOps
 import keras
 import uuid, os
 import numpy as np
@@ -14,21 +14,34 @@ def hello():
 def process_image():
     file = request.files['image']
     # Read the image via file.stream and save it in directory
+
     try:
-        img = Image.open(file.stream).convert('L')
+        img = Image.open(file.stream)
+        img = ImageOps.grayscale(img)
         img = img.resize((28, 28), Image.ANTIALIAS)
         print(img.width)
         print(img.height)
+
+            # img_inverted = Image.open('mnist_4.png')
+            # img_inverted = img_inverted.convert('L')
+            # img_inverted = img_inverted.resize((28, 28), Image.ANTIALIAS)
+            # img_inverted = ImageOps.invert(img)
+
         # predict number
         # E:\Courses\Mobile computing\mnist_nn model location
         print(tf.config.list_physical_devices('GPU'))
         model = keras.models.load_model('mnist_nn')
+    #    img = ImageOps.invert(img)
         X_test = np.asarray(img)
         print(X_test.shape)
         X_test = X_test.reshape(1,784)
+        print(X_test)
+        X_test = np.where(X_test < 120,0,X_test)
+        print(X_test)
         y_pred = model.predict(X_test)
-        print("y_pred = ", y_pred.argmax())
-        cate = y_pred
+        print("y_pred = ", y_pred)
+        cate = y_pred.argmax()
+        print("cate = ",cate)
         filename = str(uuid.uuid4())
         filepath = os.path.join(os.getcwd(), str(cate))
         if (os.path.exists(filepath)):
@@ -38,7 +51,7 @@ def process_image():
             os.mkdir(filepath)
             filepath = os.path.join(filepath, filename) + ".jpg"
             img.save(filepath)
-        return jsonify({'msg': 'success', 'size': [img.width, img.height], 'cat': cate})
+        return jsonify({'msg': 'success', 'size': [img.width, img.height], 'cat': str(cate)})
     except:
         return Response(
             "some issue with the server",
